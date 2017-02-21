569b52447ff17dbb7fbbfc6ab86841e707c2d0e3
savanafreeman <sfreeman19@hb.edu>
2017-02-21 12:16:32 -0500

    Add files via upload

---
+++ Ostrich.java
@@ -0,0 +1,25 @@
+public class Ostrich extends Bird 
+{
+  public Ostrich () 
+  {
+    super();
+    setName("Ostrich");
+    
+  }
+  
+  public void eat ()
+  {
+    super.eat();
+    System.out.print ("Gizzards");
+    
+  }
+  
+  public void speak ()
+  {
+    System.out.print("Ehhhhh-heeee-ehhhh-heeee");
+    
+  }
+  
+}
+                       
+   
